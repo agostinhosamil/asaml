fc417e7ef4acf6a765364038b94e1faef9f7e0cf
agostinhosamil <agostinhosaml832@gmail.com>
2022-10-07 08:11:22 +0100

    lint: setup eslint

--- src/app/models/AppModel.js
+++ src/app/models/AppModel.js
@@ -1,19 +1,14 @@
-const store =  {} 
+const store = {}
 
 export class AppModel extends Error {
-
   constructor (message) {
-    super (message)
+    super(message)
 
     this.name = this.constructor.name
   }
 
   static getAll () {
-    const { database } = db
-
-    console.log (database)
-
-    console.log ('Model Name => ', this.name)
+    console.log('Model Name => ', this.name)
   }
 
   static registerModuleDataObject (modelDataObject) {
