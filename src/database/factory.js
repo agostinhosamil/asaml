de05d802e69c5b4f6b01eb65ee4c18b71081bac3
agostinhosamil <agostinhosaml832@gmail.com>
2022-10-23 00:39:46 +0100

    feat(database/factory): create factory bootstrapper

---
+++ src/database/factory.js
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import path from 'path'
+
+import factory from 'factory-girl'
+
+import config from '~/config'
+
+const factoriesDirectoryPath = path.resolve(__dirname, 'factories')
+
+const factoryFilesList = fs.readdirSync(factoriesDirectoryPath)
+
+for (const factoryFile of factoryFilesList) {
+  const factoryFilePath = path.resolve(__dirname, 'factories', factoryFile)
+  const factoryModelFilePath = path.resolve(config.modelsPath, factoryFile)
+
+  try {
+    const factoryDefinitionContext = require(factoryFilePath)
+    const factoryModelModule = require(factoryModelFilePath)
+    const factoryModelModuleKeys = Object.keys(factoryModelModule)
+
+    const factoryModel = factoryModelModule[factoryModelModuleKeys[0]]
+
+    const factoryModelName = factoryModelModuleKeys[0]
+
+    factory.define(factoryModelName, factoryModel, factoryDefinitionContext.default)
+  } catch (e) {}
+}
+
+export default factory
